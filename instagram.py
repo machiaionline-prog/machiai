eea4a2f90a42e59aa4b4b0b019d873137092afdc
machiaionline-prog <machiaionline-prog@users.noreply.github.com>
2026-03-27 18:37:25 -0400

    webhook fix

---
+++ instagram.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Request
+
+app = FastAPI()
+
+VERIFY_TOKEN = "machiai"
+
+
+@app.get("/webhook")
+async def verify(request: Request):
+    mode = request.query_params.get("hub.mode")
+    token = request.query_params.get("hub.verify_token")
+    challenge = request.query_params.get("hub.challenge")
+
+    if mode == "subscribe" and token == VERIFY_TOKEN:
+        return int(challenge)
+
+    return {"error": "verification failed"}
